ad360047bbe3336dc30893481609a4ed5d168c0d
Hammad Ul Hassan <hammadulhassanqureshi@gmail.com>
2023-06-02 23:45:56 +0500

    "Project Started"

---
+++ src/Components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='container'>
+        <div className="left-container">
+
+        </div>
+        <div className="right-container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Main
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import {createBrowserHistory} from "history";
+
+const history = createBrowserHistory
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <Router history={history} >
     <App />
+     </Router>
   </React.StrictMode>,
 )
